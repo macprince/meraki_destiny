9b3eea96b9297212a4ead4532f417f4b4411921d
Dan Kuehling <macprince@gmail.com>
2019-07-28 15:08:05 -0500

    Enumerate all networks for a given org ID and exclude SM networks

--- meraki_destiny.py
+++ meraki_destiny.py
@@ -1,18 +1,20 @@
 #!/usr/local/bin/python3
 
-import argparse
-import subprocess
-import logging
-import os.path
-import sys
-import pytds
+#import argparse
+#import subprocess
+#import logging
+#import os.path
+#import sys
+#import pytds
 import pprint
 from meraki import meraki
 
 import config
 pp = pprint.PrettyPrinter(indent=4)
 
-myNetworks = meraki.getnetworklist(config.api_key, config.org_id)
-network_id = ([d for d in myNetworks if d['name'] == config.network_name][0]['id'])
+networks = meraki.getnetworklist(config.api_key, config.org_id)
+hw_networks = [net for net in networks if net['type'] != "systems manager"]
 
-devices = meraki.getnetworkdevices(config.api_key,network_id)
+for network in hw_networks:
+    print(network['type'])
+    #devices = meraki.getnetworkdevices(config.api_key,network['id'])
