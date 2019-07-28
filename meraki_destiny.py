c68d583679277f0899ee5d173dbef578127d4f3f
Dan Kuehling <macprince@gmail.com>
2019-07-28 15:45:41 -0500

    Supress printing status messages when calling the Meraki API

--- meraki_destiny.py
+++ meraki_destiny.py
@@ -12,9 +12,10 @@ from meraki import meraki
 import config
 pp = pprint.PrettyPrinter(indent=4)
 
-networks = meraki.getnetworklist(config.api_key, config.org_id)
+networks = meraki.getnetworklist(config.api_key, config.org_id,suppressprint=True)
 hw_networks = [net for net in networks if net['type'] != "systems manager"]
 
 for network in hw_networks:
-    print(network['type'])
-    #devices = meraki.getnetworkdevices(config.api_key,network['id'])
+    devices = meraki.getnetworkdevices(config.api_key,network['id'],suppressprint=True)
+    for device in devices:
+        print(device['serial'])
