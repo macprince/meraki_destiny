38923b52b7b99fb2e9e7c744bc1bf97c26460633
Dan Kuehling <dkuehling@district158.org>
2019-07-29 23:21:13 -0500

    Collect serials from Meraki API into a list for later database lookup

--- meraki_destiny.py
+++ meraki_destiny.py
@@ -1,21 +1,21 @@
 #!/usr/local/bin/python3
 
-#import argparse
-#import subprocess
-#import logging
-#import os.path
-#import sys
-#import pytds
+import argparse
+import subprocess
+import logging
+import os.path
+import sys
+import pytds
 import pprint
 from meraki import meraki
 
 import config
 pp = pprint.PrettyPrinter(indent=4)
+serials=[]
 
 networks = meraki.getnetworklist(config.api_key, config.org_id,suppressprint=True)
 hw_networks = [net for net in networks if net['type'] != "systems manager"]
-
 for network in hw_networks:
     devices = meraki.getnetworkdevices(config.api_key,network['id'],suppressprint=True)
     for device in devices:
-        
+        serials.append(device['serial'])
